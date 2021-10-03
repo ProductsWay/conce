acb152d6a682d33ce39cf3f7de128e4355c4a018
Huynh Duc Dung <dunghd.it@gmail.com>
2021-10-03 23:17:45 +0800

    :sparkles: add superbaes quickstart app
    Flutter base with authentication

---
+++ mobile/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:conce_app/pages/account_page.dart';
+import 'package:conce_app/pages/login_page.dart';
+import 'package:conce_app/pages/splash_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // TODO: read from .env file
+  await Supabase.initialize(
+    url: 'https://gjxvzzhqvecxdtfqvdah.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzI1MTU3NSwiZXhwIjoxOTQ4ODI3NTc1fQ.l1LfM1UfLaoHUXPvsEPh1jpV-yteP-1KGALbxtnQ_l4',
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Conce App',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.green,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            onPrimary: Colors.white,
+            primary: Colors.green,
+          ),
+        ),
+      ),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (_) => const SplashPage(),
+        '/login': (_) => const LoginPage(),
+        '/account': (_) => const AccountPage(),
+      },
+    );
+  }
+}
