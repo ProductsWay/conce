2fa1b2ee8a46f9c6431fe4121ad01015f31aca1f
Huynh Duc Dung <dunghd.it@gmail.com>
2021-10-23 22:44:15 +0800

    :truck: rename example to user api

--- packages/api/src/routes/user/index.ts
+++ packages/api/src/routes/user/index.ts
@@ -1,9 +1,9 @@
 import { FastifyPluginAsync } from "fastify";
 
-const example: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
+const user: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
   fastify.get("/", async (_request, reply) => {
     reply.send({ hello: "world" });
   });
 };
 
-export default example;
+export default user;
